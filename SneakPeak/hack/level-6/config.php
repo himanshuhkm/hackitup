a2da892f7411a7a40d5cf34cda9d20267692e323
himanshuhkm <hackbrohkm@gmail.com>
2020-03-11 23:05:02 +0530

    level 6

--- SneakPeak/hack/level-6/config.php
+++ SneakPeak/hack/level-6/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'sneakpeak');
-define('DB_PASSWORD', 'NeverTakeItEasy');
-define('DB_NAME', 'hackitupsneakpeak');
+define('DB_USERNAME', 'cheesetouched');
+define('DB_PASSWORD', 'iamhardwell');
+define('DB_NAME', 'hackitup');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
