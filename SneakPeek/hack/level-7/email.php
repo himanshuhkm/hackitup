a6dfd6f0735c21511e22829e6ae036e686255e8f
himanshuhkm <himanshuhkmbro@gmail.com>
2020-03-13 14:42:33 +0530

    level 7

--- SneakPeek/hack/level-7/email.php
+++ SneakPeek/hack/level-7/email.php
@@ -4,13 +4,14 @@
    $myusername = $_POST['username'];
    $mypassword = $_POST['password']; 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
-           echo "https://usamaejaz.com/cloudflare-email-decoding/";
-            header('Session: try to mess with your body');
+           echo "Well great! Now that you are here dont forget to check me out :)";
+            header('Courtesy: https://usamaejaz.com/cloudflare-email-decoding/');
+            header('Session: i have nothing to do with email');
             header('Cookie: no need');
             header('Key: 543938142127353735313e352e7a373b38');
             header('Mode: CTR');
             header('Base: 64');
-            header('');
+            
 
        }
    else {
